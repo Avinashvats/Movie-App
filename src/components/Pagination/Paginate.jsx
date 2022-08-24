c89992d36efb9db811bb031d272915cbb19e9f90
Avinash Vats <84141727+Avinashvats@users.noreply.github.com>
2022-08-24 15:53:26 +0530

    Add files via upload

--- src/components/Pagination/Paginate.jsx
+++ src/components/Pagination/Paginate.jsx
@@ -1,1 +1,37 @@
+import React, { useEffect, useRef } from "react";
 
+export default function Paginate(props) {
+  const observingDiv = useRef();
+
+  useEffect(() => {
+    const observingElement = observingDiv.current;
+    if (!observingElement) return;
+
+    const observer = new IntersectionObserver((data) => {
+      const isIntersecting = data[0].isIntersecting;
+
+      if (props.onIntersection) props.onIntersection(isIntersecting);
+    });
+
+    observer.observe(observingElement);
+
+    return () => {
+      observer.unobserve(observingElement);
+    };
+  }, []);
+
+  return (
+    <div style={{ position: "relative" }}>
+      {props.children}
+
+      <div
+        ref={observingDiv}
+        style={{
+          position: "absolute",
+          buttom: "0",
+          height: `${window.innerHeight * 1.5}px`,
+        }}
+      />
+    </div>
+  );
+}
