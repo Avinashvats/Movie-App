c96e149f5bfae234e114f1ec52fb6d0e1a34d447
Avinash Vats <84141727+Avinashvats@users.noreply.github.com>
2022-08-24 15:49:30 +0530

    Add files via upload

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./MovieCard.module.css";
 
+const imagePrefixUrl = "http://image.tmdb.org/t/p/w500";
+export default function MovieCard(props) {
+  const movie = props?.movie;
+  return (
+    <Link
+      to={`/movie/${movie.id}`}
+      // target="_blank"
+      className={styles.container}
+    >
+      <img
+        src={`${imagePrefixUrl}${props.movie?.poster_path}`}
+        alt={movie?.title}
+      />
+      <p>{movie?.title}</p>
+    </Link>
+  );
+}
