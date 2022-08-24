1a672ce9a6d4d650f642303d49e28d821a2d3553
Avinash Vats <84141727+Avinashvats@users.noreply.github.com>
2022-08-24 15:52:53 +0530

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,1 +1,24 @@
+import React from "react";
+// import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={styles.container}>
+      <Link to="/" className={styles.logo}>
+        MovieMania
+      </Link>
+      <div className={styles.right}>
+        {/* <div className={styles.search}>
+          <input type="text" placeholder="Search movie..." />
+          <FaSearch />
+        </div> */}
+        <p className={styles.link}>
+          <Link to="/explore">Explore</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
 
